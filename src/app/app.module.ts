53fc10eeb9da3655785cd445687c6e9651e1c772
desarrolloMercury <desarrollosistemas@prevrenal.org>
2024-08-19 16:58:23 -0500

    SSR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
-import { NgxSpinnerService } from 'ngx-spinner';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderModule } from '@app/components/header/header.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
-import { HttpClientModule } from '@angular/common/http';
-import { HeaderModule } from '@app/components/header/header.module';
 
-import { NgxSpinnerModule } from 'ngx-spinner';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     GraphQLModule,
     HttpClientModule,
@@ -24,8 +23,6 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     BrowserAnimationsModule,
     FontAwesomeModule,
   ],
-  exports: [NgxSpinnerModule],
-  providers: [],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -1,18 +1,24 @@
 import { Personaje } from '@app/models/personaje';
 import { ReplaySubject } from 'rxjs';
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LocalStorageService implements OnInit {
   private personajesFavoritosSub = new ReplaySubject<Personaje[]>(1);
+  private isBrowser: boolean;
   personajesFavritos$ = this.personajesFavoritosSub.asObservable();
   fav: string = 'PersonajesFavoritos';
 
-  constructor() {
-    let favoritoStorage = JSON.parse(localStorage.getItem(this.fav)!);
-    if (!favoritoStorage) localStorage.setItem(this.fav, JSON.stringify([]));
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(this.platformId);
+
+    if (this.isBrowser) {
+      let favoritoStorage = JSON.parse(localStorage.getItem(this.fav)!);
+      if (!favoritoStorage) localStorage.setItem(this.fav, JSON.stringify([]));
+    }
   }
 
   async ngOnInit() {
@@ -22,6 +28,8 @@ export class LocalStorageService implements OnInit {
   }
 
   async getPersonajesFavoritos(): Promise<Personaje[]> {
+    if (!this.isBrowser) return [];
+
     let personajesFavoritos = JSON.parse(localStorage.getItem(this.fav)!);
 
     this.personajesFavoritosSub.next(personajesFavoritos);
@@ -29,20 +37,27 @@ export class LocalStorageService implements OnInit {
     return personajesFavoritos;
   }
 
-  private async quitarFavorito(id: string) {
-    let listaFavoritos = await this.getPersonajesFavoritos(),
-      personajes = listaFavoritos.filter((P) => P.id != id);
+  private async quitarFavorito(id: string): Promise<void> {
+    let listaFavoritos = await this.getPersonajesFavoritos();
 
-    localStorage.setItem(this.fav, JSON.stringify([...personajes]));
-    this.personajesFavoritosSub.next([...personajes]);
+    if (!listaFavoritos.length) return;
+
+    let personajes = listaFavoritos.filter((P) => P.id != id);
+
+    if (this.isBrowser) {
+      localStorage.setItem(this.fav, JSON.stringify([...personajes]));
+      this.personajesFavoritosSub.next([...personajes]);
+    }
   }
 
-  private async agregarFavorito(personaje: Personaje) {
+  private async agregarFavorito(personaje: Personaje): Promise<void> {
     let listaFavoritos = await this.getPersonajesFavoritos();
-    localStorage.setItem(
-      this.fav,
-      JSON.stringify([...listaFavoritos, personaje])
-    );
+
+    if (this.isBrowser)
+      localStorage.setItem(
+        this.fav,
+        JSON.stringify([...listaFavoritos, personaje])
+      );
 
     this.personajesFavoritosSub.next([...listaFavoritos, personaje]);
   }
@@ -50,7 +65,7 @@ export class LocalStorageService implements OnInit {
   async cambiarFavorito(personaje: Personaje): Promise<void> {
     let { id } = personaje,
       listaFavoritos = await this.getPersonajesFavoritos(),
-      find = !!listaFavoritos.find((fav: Personaje) => fav.id === id);
+      find = listaFavoritos.find((fav: Personaje) => fav.id === id);
 
     find
       ? this.quitarFavorito(id).catch((err) =>
